8ac0c7bc2b8b3685a783f52ca47d255612e6ae4f
jorge555-hue <jorge55544@gmail.com>
2026-02-27 15:34:35 -0500

    Update uber_nyc_pickpus.py

--- uber_nyc_pickpus.py
+++ uber_nyc_pickpus.py
@@ -24,3 +24,10 @@ data = load_data(10000)
 
 # Notify the reader that the data was successfully loaded. 
 data_load_state.text("Done! (using st.cache_data)")
+
+Chris Santo
+scottsdalecc.edu
+3:33 PM
+st.subheader('Raw data')
+
+st.write(data)
